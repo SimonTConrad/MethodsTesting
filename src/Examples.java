e3f20da6365347efc632bbbd31ada45960da62c9
Kronos <42975620+SimonTConrad@users.noreply.github.com>
2019-03-01 13:04:02 -0500

    Add files via upload

---
+++ src/Examples.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Examples {
+
+	public static void main(String[] args) 
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.print("Please enter your name:  ");
+		String name = in.nextLine();
+		
+		greetings(name);
+		
+		System.out.print("Enter a number to double:  ");
+		int myNum =  in.nextInt();
+		in.nextLine();
+		
+		int myDouble = doubleInt(myNum);
+		System.out.println("My number Doubled: " + myDouble);
+		
+		System.out.println(sumThree(1,2,3));
+	}
+	public static void greetings(String arg1)
+	{
+		System.out.println("Hello, " + arg1 );
+		System.out.println("I hope you have a nice day");
+	}
+	
+	public static int doubleInt(int num)
+	{
+		int newNum = num * 2;
+		return newNum;
+	}
+	
+	public static int sumThree(int num1, int num2, int num3)
+	{
+		int sum = num1 + num2 + num3;
+		return sum;
+	}
+	
+	
+	
+	
+	
+}
